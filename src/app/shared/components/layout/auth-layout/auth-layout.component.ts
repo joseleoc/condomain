a67aa72fde79594a96d3e08c727721b7d4cd3855
joseleoc <joseleoc123@gmail.com>
2026-06-11 16:25:50 -0400

    back button in auth layout

--- src/app/shared/components/layout/auth-layout/auth-layout.component.ts
+++ src/app/shared/components/layout/auth-layout/auth-layout.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { Component, input } from '@angular/core';
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
   styleUrls: ['./auth-layout.component.scss'],
-  imports: [IonContent],
+  imports: [IonContent, IonHeader, IonToolbar, IonBackButton, IonButtons],
 })
-export class AuthLayoutComponent {}
+export class AuthLayoutComponent {
+  // --- Inputs ---
+  showBackButton = input<boolean>(true);
+}
