c04804f6f7f65f5d082d5da3915ebb7a2f7ab59a
joseleoc <joseleoc123@gmail.com>
2026-06-13 23:57:11 -0400

    feat(condominium): enhance create condominium form with currency field and validation

--- src/app/features/condominium/components/create-condominium-form/create-condominium-form.component.ts
+++ src/app/features/condominium/components/create-condominium-form/create-condominium-form.component.ts
@@ -1,14 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CreateCondominiumData } from '@core/services/condominium/condominium.types';
+import { IonInput, IonButton, IonTextarea } from '@ionic/angular/standalone';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
+import { map } from 'rxjs/internal/operators/map';
+
+interface CreateCondominiumFormControls {
+  name: FormControl<string>;
+  address: FormControl<string>;
+  currency: FormControl<string>;
+}
 
 @Component({
   selector: 'app-create-condominium-form',
   templateUrl: './create-condominium-form.component.html',
   styleUrls: ['./create-condominium-form.component.scss'],
+  imports: [
+    ReactiveFormsModule,
+    IonInput,
+    IonButton,
+    IonTextarea,
+    TranslocoModule,
+    AsyncPipe,
+  ],
 })
-export class CreateCondominiumFormComponent   {
+export class CreateCondominiumFormComponent {
+  // --- Dependencies ---
+  private translocoService = inject(TranslocoService);
 
-  constructor() { }
+  // --- Outputs ---
+  submitCreateCondominiumForm = output<CreateCondominiumData>();
 
-  ngOnInit() {}
+  // --- Form ---
+  createCondominiumForm = new FormGroup<CreateCondominiumFormControls>({
+    name: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(3)],
+      nonNullable: true,
+    }),
+    address: new FormControl('', { nonNullable: true }),
+    currency: new FormControl('USD', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
 
+  //--- Properties ---
+  nameError$ = this.createCondominiumForm.controls.name.statusChanges.pipe(
+    map(() => {
+      const nameControl = this.createCondominiumForm.controls.name;
+      if (nameControl.hasError('required')) {
+        return this.translocoService.translate('validation.required');
+      }
+      if (nameControl.hasError('minlength')) {
+        return this.translocoService.translate('validation.minLength', {
+          length: nameControl.getError('minlength').requiredLength,
+        });
+      }
+      return null;
+    }),
+  );
+
+  currencyError$ =
+    this.createCondominiumForm.controls.currency.statusChanges.pipe(
+      map(() => {
+        const currencyControl = this.createCondominiumForm.controls.currency;
+        if (currencyControl.hasError('required')) {
+          return this.translocoService.translate('validation.required');
+        }
+        return null;
+      }),
+    );
+
+  // --- Methods ---
+  onSubmit() {
+    console.log(this.createCondominiumForm.value);
+    if (this.createCondominiumForm.valid) {
+      // Handle form submission
+    }
+  }
 }
