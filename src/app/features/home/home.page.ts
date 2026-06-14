26e36a64b4e02d799e30e63ed468008f259c5511
joseleoc <joseleoc123@gmail.com>
2026-06-14 14:03:00 -0400

    feat(home): update home page layout with new animated button and logout functionality

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -1,20 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { Auth } from '@core/services/auth/auth';
 import { MainLayoutComponent } from '@shared/components/layout/main-layout/main-layout.component';
 import { IonButton } from '@ionic/angular/standalone';
 import { Condominium } from '@core/services/condominium/condominium';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NewCondoAnimatedBtnComponent } from '@shared/components/new-condo-animated-btn/new-condo-animated-btn.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [MainLayoutComponent, IonButton],
+  imports: [MainLayoutComponent, IonButton, NewCondoAnimatedBtnComponent],
 })
 export class HomePage {
-  private authService = inject(Auth);
-  constructor() {}
+  private condominiumService = inject(Condominium);
 
-  logOut() {
-    this.authService.signOut();
-  }
+  // --- Properties ---
+  activeCondominium = toSignal(this.condominiumService.activeCondominium$);
+  // --- Methods ---
 }
--- src/app/shared/components/new-condo-animated-btn/new-condo-animated-btn.component.ts
+++ src/app/shared/components/new-condo-animated-btn/new-condo-animated-btn.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-new-condo-animated-btn',
   templateUrl: './new-condo-animated-btn.component.html',
   styleUrls: ['./new-condo-animated-btn.component.scss'],
+  imports: [IonButton, TranslocoPipe, IonIcon],
 })
-export class NewCondoAnimatedBtnComponent  implements OnInit {
-
-  constructor() { }
+export class NewCondoAnimatedBtnComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
