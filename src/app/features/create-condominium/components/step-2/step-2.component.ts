d069a4da1ba52b88cfd9ca77a38d4ab308565118
joseleoc <joseleoc123@gmail.com>
2026-06-15 16:36:38 -0400

    feat(condominium): add step 2 component with HTML, SCSS, and unit tests

---
+++ src/app/features/create-condominium/components/step-2/step-2.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-2',
+  templateUrl: './step-2.component.html',
+  styleUrls: ['./step-2.component.scss'],
+})
+export class Step2Component  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
